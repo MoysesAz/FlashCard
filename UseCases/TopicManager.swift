b9cef80f3e873e2b40f57d938f2ac4ba9cce0875
Moyses Azevedo <m.mirazev@gmail.com>
2023-05-30 13:44:56 -0300

    :sparkles: feat: add verifyErros

--- UseCases/TopicManager.swift
+++ UseCases/TopicManager.swift
@@ -9,20 +9,24 @@ import Foundation
 import Domain
 
 public class TopicManager: AddTopicProtocol, DeleteTopicProtocol, AddCardProtocol, DeleteCardProtocol {
-    private var topics: [Topic] = []
-    private var pointerTopic: Int = 0
+    public var topics: [Topic] = []
+    public var pointerTopic: Int = 0
 
-    public func addCard(tittle: String, description: String) {
+    public func addCard(tittle: String, description: String) throws {
+        try verifyTopic()
         let newCard = Card(UUID().uuidString, tittle: tittle, description: description)
         topics[pointerTopic].cards.append(newCard)
     }
 
-    public func addCard(tittle: String, imageData: Data) {
+    public func addCard(tittle: String, imageData: Data) throws {
+        try verifyTopic()
         let newCard = Card(UUID().uuidString, tittle: tittle, image: imageData)
         topics[pointerTopic].cards.append(newCard)
     }
 
-    public func deleteCardProtocol(index: Int) {
+    public func deleteCard(index: Int) throws {
+        try verifyTopic()
+        try verifyCard(cardIndex: index)
         topics[pointerTopic].cards.remove(at: index)
     }
 
@@ -31,9 +35,26 @@ public class TopicManager: AddTopicProtocol, DeleteTopicProtocol, AddCardProtoco
         topics.append(newTopic)
     }
 
-    public func deleteTopic(index: Int) {
+    public func deleteTopic(index: Int) throws {
+        try verifyTopic()
         topics.remove(at: index)
     }
 
+    private func verifyTopic() throws {
+        guard topics.isEmpty else {
+            throw TopicManagerError.topicIsEmpty
+        }
+        guard pointerTopic > topics.count || pointerTopic < 0 else {
+            throw TopicManagerError.indexNotCompatibleWithTopic
+        }
+    }
 
+    private func verifyCard(cardIndex: Int) throws {
+        guard topics[pointerTopic].cards.isEmpty else {
+            throw TopicManagerError.cardsIsEmpty
+        }
+        guard cardIndex > topics[pointerTopic].cards.count || cardIndex < 0 else {
+            throw TopicManagerError.indexNotCompatibleWithCards
+        }
+    }
 }
