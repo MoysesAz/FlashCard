e22c461217911ea4af72c8066ed99dec72ae732b
Moyses Azevedo <m.mirazev@gmail.com>
2023-05-30 14:15:54 -0300

    :recycle: refactor: modify variable topics

--- UseCases/TopicManager.swift
+++ UseCases/TopicManager.swift
@@ -9,51 +9,56 @@ import Foundation
 import Domain
 
 public class TopicManager: AddTopicProtocol, DeleteTopicProtocol, AddCardProtocol, DeleteCardProtocol {
-    public var topics: [Topic] = []
+    private var _topics: [Topic] = []
+    public var topics: [Topic] {
+        get { return _topics }
+        set { _ = newValue }
+    }
+
     public var pointerTopic: Int = 0
 
     public func addCard(tittle: String, description: String) throws {
         try verifyTopic()
         let newCard = Card(UUID().uuidString, tittle: tittle, description: description)
-        topics[pointerTopic].cards.append(newCard)
+        _topics[pointerTopic].cards.append(newCard)
     }
 
     public func addCard(tittle: String, imageData: Data) throws {
         try verifyTopic()
         let newCard = Card(UUID().uuidString, tittle: tittle, image: imageData)
-        topics[pointerTopic].cards.append(newCard)
+        _topics[pointerTopic].cards.append(newCard)
     }
 
     public func deleteCard(index: Int) throws {
         try verifyTopic()
         try verifyCard(cardIndex: index)
-        topics[pointerTopic].cards.remove(at: index)
+        _topics[pointerTopic].cards.remove(at: index)
     }
 
     public func addTopic(name: String) {
         let newTopic = Topic(UUID().uuidString, name: name, cards: [])
-        topics.append(newTopic)
+        _topics.append(newTopic)
     }
 
     public func deleteTopic(index: Int) throws {
         try verifyTopic()
-        topics.remove(at: index)
+        _topics.remove(at: index)
     }
 
     private func verifyTopic() throws {
-        guard topics.isEmpty else {
+        guard _topics.isEmpty else {
             throw TopicManagerError.topicIsEmpty
         }
-        guard pointerTopic > topics.count || pointerTopic < 0 else {
+        guard pointerTopic > _topics.count || pointerTopic < 0 else {
             throw TopicManagerError.indexNotCompatibleWithTopic
         }
     }
 
     private func verifyCard(cardIndex: Int) throws {
-        guard topics[pointerTopic].cards.isEmpty else {
+        guard _topics[pointerTopic].cards.isEmpty else {
             throw TopicManagerError.cardsIsEmpty
         }
-        guard cardIndex > topics[pointerTopic].cards.count || cardIndex < 0 else {
+        guard cardIndex > _topics[pointerTopic].cards.count || cardIndex < 0 else {
             throw TopicManagerError.indexNotCompatibleWithCards
         }
     }
