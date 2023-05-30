492719e7aa6251070e0d8f88fa9ea8fa4e022c9d
Moyses Azevedo <m.mirazev@gmail.com>
2023-05-30 14:15:09 -0300

    :test_tube: test: add test_addTopic

--- UseCasesTests/TopicManagerTests.swift
+++ UseCasesTests/TopicManagerTests.swift
@@ -9,30 +9,10 @@ import XCTest
 @testable import UseCases
 
 class TopicManagerTests: XCTestCase {
-
-    func
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func teste_addTopic() {
+        let sut = TopicManager()
+        sut.addTopic(name: "topico 1")
+        let result = sut.topics.count
+        XCTAssertEqual(result, 1, "Era esperado existir 1 item na lista")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
