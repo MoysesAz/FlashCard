c7e5c62bd3ff214e777af234b1c70fd8cad3c7cb
Moyses Azevedo <m.mirazev@gmail.com>
2023-05-31 14:05:09 -0300

    :test_tube: test: add test_deleteCard

--- UseCasesTests/TopicManagerTests.swift
+++ UseCasesTests/TopicManagerTests.swift
@@ -6,18 +6,37 @@
 //
 
 import XCTest
+import Domain
 @testable import UseCases
 
 class TopicManagerTests: XCTestCase {
+    var sut: TopicManager!
+
+    override func setUpWithError() throws {
+        sut = TopicManager()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
     func teste_addTopic() {
-        let sut = TopicManager()
-        sut.addTopic(name: "Solid")
+        let nameTopic = "Solid"
+
+        sut.addTopic(name: nameTopic)
         let result = sut.topics.count
         XCTAssertEqual(result, 1)
     }
 
+    func teste_addTopic_values() {
+        let nameTopic = "Solid"
+
+        sut.addTopic(name: nameTopic)
+        let result = sut.topics
+        XCTAssertEqual(result.count, 1)
+    }
+
     func teste_deleteTopic() {
-        let sut = TopicManager()
         sut.addTopic(name: "Solid")
 
         do {
@@ -27,6 +46,46 @@ class TopicManagerTests: XCTestCase {
         }
 
         let result = sut.topics.count
+        XCTAssertEqual(result, 0)
+    }
+
+    func teste_addCardWithDescription() {
+        sut.addTopic(name: "Solid")
+        sut.pointerTopic = 0
+
+        do {
+            try sut.addCard(title: "Formula da energia", description: "E = mcˆ2")
+        } catch {
+            XCTFail("Erro inesperado: \(error)")
+        }
+        let result = sut.topics[0].cards.count
+        XCTAssertEqual(result, 1)
+    }
+
+    func teste_addCardWithData() {
+        sut.addTopic(name: "Solid")
+        sut.pointerTopic = 0
+
+        do {
+            try sut.addCard(title: "Formula da energia", imageData: Data())
+        } catch {
+            XCTFail("Erro inesperado: \(error)")
+        }
+        let result = sut.topics[0].cards.count
+        XCTAssertEqual(result, 1)
+    }
+
+    func teste_DeleteCard() {
+        sut.addTopic(name: "Solid")
+        sut.pointerTopic = 0
+
+        do {
+            try sut.addCard(title: "Formula da energia", imageData: Data())
+            try sut.deleteCard(indexCard: 0)
+        } catch {
+            XCTFail("Erro inesperado: \(error)")
+        }
+        let result = sut.topics[0].cards.count
         XCTAssertEqual(result, 0)
     }
 }
