dd34fcbd66a066526045dbb65fc256e128d9ac45
Moyses Azevedo <m.mirazev@gmail.com>
2023-06-01 22:58:00 -0300

    add TopicModel

---
+++ Domain/Model/TopicModel.swift
@@ -0,0 +1,13 @@
+//
+//  TopicModel.swift
+//  Domain
+//
+//  Created by Moyses Miranda do Vale Azevedo on 01/06/23.
+//
+
+import Foundation
+
+struct TopicModel {
+    var id: UUID
+    var name: String
+}
