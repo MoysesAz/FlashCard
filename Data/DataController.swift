d9c1a80d7047ce83ec53a45360bf7752db9c5231
Moyses Azevedo <m.mirazev@gmail.com>
2023-06-03 19:46:13 -0300

    add component data

---
+++ Data/DataController.swift
@@ -0,0 +1,22 @@
+//
+//  DataController.swift
+//  Data
+//
+//  Created by Moyses Miranda do Vale Azevedo on 01/06/23.
+//
+
+import CoreData
+
+public class DataController: ObservableObject {
+    public static let shared = DataController()
+    public let container = NSPersistentContainer(name: "Deck")
+
+    public init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+
+}
