e5132b468847a859bedb9e6b817f93defc152de9
Moyses Azevedo <m.mirazev@gmail.com>
2023-07-27 13:58:30 -0300

    :bug: fix: FormsSubtopic bug var pointer

---
+++ Presentation/SubTopics/FormEditSubTopic.swift
@@ -0,0 +1,20 @@
+//
+//  FormEditSubTopic.swift
+//  Presentation
+//
+//  Created by Moyses Miranda do Vale Azevedo on 26/07/23.
+//
+
+import SwiftUI
+
+struct FormEditSubTopic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FormEditSubTopic_Previews: PreviewProvider {
+    static var previews: some View {
+        FormEditSubTopic()
+    }
+}
