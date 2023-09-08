5305c87ea930c62658f592b40629d6874eb4329f
Moyses Azevedo <m.mirazev@gmail.com>
2023-09-08 10:43:08 -0300

    :sparkles: feat: version 1.3.0

--- Presentation/PresentationApp.swift
+++ Presentation/PresentationApp.swift
@@ -10,17 +10,16 @@ import Data
 import GoogleMobileAds
 import UIKit
 
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers =
-//            [ "2077ef9a63d2b398840261c8221a0c9b" ]
-//        return true
-//    }
-//}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
 
 @main
 struct PresentationApp: App {
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
         WindowGroup {
