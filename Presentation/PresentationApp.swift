286e2013f64dfe1915e3bc88e26dedcd5bb0e581
Moyses Azevedo <m.mirazev@gmail.com>
2023-09-09 11:48:38 -0300

    :sparkles: feat: rename branch 1.3.0 by 1.5.0

--- Presentation/PresentationApp.swift
+++ Presentation/PresentationApp.swift
@@ -10,17 +10,21 @@ import Data
 import GoogleMobileAds
 import UIKit
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        return true
-    }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        GADMobileAds.sharedInstance().start(completionHandler: nil)
+//        return true
+//    }
+//}
 
 @main
 struct PresentationApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
+
     var body: some Scene {
         WindowGroup {
            TopicView()
